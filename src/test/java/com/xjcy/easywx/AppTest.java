ab0a3da6aada284c62785158ae18a9d277ac36a2
zml <441430565@qq.com>
2018-04-06 11:58:23 +0800

    update encode

--- src/test/java/com/xjcy/easywx/AppTest.java
+++ src/test/java/com/xjcy/easywx/AppTest.java
@@ -2,12 +2,10 @@ package com.xjcy.easywx;
 
 import java.util.Date;
 
-import junit.framework.TestCase;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
+public class AppTest {
 	public static void main(String[] args) {
 		System.out.println(new Date().getTime() / 1000);
 		System.out.println(System.currentTimeMillis() / 1000);
